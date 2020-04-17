20978afecaf8308638d7bb1fdf6a86a941d97453
Mbenyedder <62854181+Mbenyedder@users.noreply.github.com>
2020-04-17 09:28:38 +0200

    Update data.py

--- data.py
+++ data.py
@@ -7,14 +7,13 @@ produit = pd.read_csv("C:/Users/hp/PycharmProjects/Python/ML_1/Kaggle/Instacart/
 df_produit = produit[["department_id", "product_id"]]
 
 
-#Lecture des données
 
 df_commande = pd.read_csv("C:/Users/hp/PycharmProjects/Python/ML_1/Kaggle/Instacart/Data/orders.csv")
 
 
 df_commande = df_commande [["order_id", "user_id"]]
 
-N=100  #On commence à tester pour 100 clients
+N=1000
 
 def commande(n_clients):
    return df_commande[df_commande['user_id'].isin(range(N+1))]
@@ -22,12 +21,9 @@ def commande(n_clients):
 commande_df = commande(N)
 
 
-#Order products prior
-#Lecture des données + on garde les données qui nous interesse
 
 commande_produit = pd.read_csv("C:/Users/hp/PycharmProjects/Python/ML_1/Kaggle/Instacart/Data/order_products__prior.csv")
 
-#print(commande_produit .columns)
 
 df_commande_produit =commande_produit [['order_id', 'product_id']]
 
